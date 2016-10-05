aaab5aa81d120b8872770b76bf0d1a350682d451
Samuel Garcia <sam/garcia.die@gmail.com>
2016-10-05 18:12:07 +0200

    make gui loss parameters Irino like.

--- hearinglosssimulator/gui/lossparameters.py
+++ hearinglosssimulator/gui/lossparameters.py
@@ -5,247 +5,176 @@ import numpy as np
 import scipy.interpolate
 import json
 
+from collections import OrderedDict
+
 from hearinglosssimulator.gui.guitools import MplCanvas
 
-_default_bands = [ {'freq' : np.ceil(100*((2**(1./3.))**i)), 'db_loss': 0.}  for i in range(20) ]
 
 
-class Band(QtGui.QWidget):
-    valueChanged = QtCore.Signal()
+#~ _default_bands = [ {'freq' : np.ceil(100*((2**(1./3.))**i)), 'db_loss': 0.}  for i in range(20) ]
+n = 7
+freqs = [ 125*2**i  for i in range(n) ]
+default_loss = [ {'freq' : f, 'db_loss': 0.}  for f in freqs ]
+
+compression_loss_preset = OrderedDict([
+    ('100%', [-6.7406, 2.4563, 4.7537, 5.7118, 1.0655, -3.4335, -7.2814]),
+    ('67%', [-0.3463, 10.0101, 17.8573, 19.9751, 16.8954, 14.5161, 5.1201]),
+    ('50%', [2.5999, 14.3605, 23.7777, 26.8029, 24.7069, 23.5780, 10.6923]),
+    ('33%', [5.4801,  18.5666, 28.9101, 32.6974, 31.2244, 30.0204, 16.0619]),
+    ('0%', [10.5790, 24.9903, 36.0186, 40.5679, 38.9283, 36.5154, 25.2229]),
+])
+compression_ratio = OrderedDict(zip(('0%', '33%', '50%', '67%','100%'), (0, 1/3., 1/2., 2/3., 1.)))
+
+
+hearing_level_preset = OrderedDict([
+    ('example 1' , [10, 4, 10, 13, 48, 58, 79]),
+    ('60 years', [10, 15, 15, 15, 25, 35, 43]),
+    ('80 years', [25, 30, 32, 28, 38, 50, 60]),
+    ('Otosclerosis', [50, 55, 50, 50, 40, 25, 20]),
+    ('Noise-induced', [15, 10, 15, 10, 10, 40, 20]),
+])
+
+
+
+def plot_hearingloss(ax, hearing_level, compression_loss):
+        hearing_level = np.array(hearing_level)
+        compression_loss = np.array(compression_loss)
+        
+        lines ={} # line handles 
+        
+        ax.clear()
+        x = np.arange(n)+.5
+        
+        ax.axhline(0, color='k', lw=2)
+        
+        compression_loss_clip = np.amin([hearing_level, compression_loss], axis=0)
+        
+        lines['hearing_level'], = ax.plot(x, hearing_level, color='#000000', marker='o', markersize=12, lw=2, ls='-', markerfacecolor='w')
+        lines['normal'],= ax.plot(x, compression_loss_preset['100%'], color='#2EFE2E', marker='8', markersize=8, lw=2, ls='-')
+        lines['compression_loss'], = ax.plot(x, compression_loss, color='#00FFFF', marker='s', markersize=8, lw=.5, ls='--')
+        lines['compression_loss_clip'], = ax.plot(x, compression_loss_clip, color='#FF00FF', marker='D', markersize=8, lw=2, ls='-')
+        
+        ax.set_title('Frequency (Hz)')
+        ax.set_ylabel('Hearing Level (dB)')
+        ax.grid(True)
+        ax.set_xticks([i+0.5 for i in range(n)])
+        ax.set_xticklabels(freqs)
+        ax.set_yticks(np.arange(-20., 130., 10.))
+        ax.set_xlim(0,n)
+        ax.set_ylim(120., -20.)
+        
+        return lines
+
+class HearingLossParameter(QtGui.QWidget):
     def __init__(self, parent = None):
         QtGui.QWidget.__init__(self, parent)
         mainlayout  = QtGui.QVBoxLayout()
         self.setLayout(mainlayout)
-        mainlayout.setContentsMargins(1,1,1,1)
-        
-        self.db_label = QtGui.QLabel(u'')
-        mainlayout.addWidget(self.db_label)
-        self.slider_db_loss = QtGui.QSlider(orientation = QtCore.Qt.Vertical, minimum = -60, maximum = 0, minimumHeight = 100)
-        mainlayout.addWidget(self.slider_db_loss)
-        self.spinbox_freq = QtGui.QSpinBox(minimum = 50, maximum = 8000, minimumWidth = 5, maximumWidth = 55, maximumHeight = 18)
-        font = QtGui.QFont()
-        font.setPointSize(7)
-        self.spinbox_freq.setFont(font)
-        mainlayout.addWidget(self.spinbox_freq)
-        
-        #~ mainlayout.setAlignment(QtCore.Qt.AlignHCenter)
-        mainlayout.setAlignment(self.slider_db_loss, QtCore.Qt.AlignHCenter)
-        mainlayout.setAlignment(self.db_label, QtCore.Qt.AlignHCenter)
-        
-        self.slider_db_loss.valueChanged.connect(self.refresh_label)
-        self.slider_db_loss.valueChanged.connect(self.valueChanged.emit)
-        self.spinbox_freq.valueChanged.connect(self.valueChanged.emit)
-        
-        self.refresh_label(0)
-    
-    def set(self, freq = 1000., db_loss = 0.):
-        self.slider_db_loss.setValue(int(db_loss))
-        self.spinbox_freq.setValue(int(freq))
-        
-    def get(self):
-        return {
-                        'db_loss' : float(self.slider_db_loss.value()),
-                        'freq' : float(self.spinbox_freq.value()),
-                    }
-    
-    def refresh_label(self, value):
-        self.db_label.setText(u'{} dB'.format(value))
 
 
-class OneEq(QtGui.QWidget):
-    valueChanged = QtCore.Signal()
-    def __init__(self, parent = None, band_values = _default_bands ):
-        QtGui.QWidget.__init__(self, parent)
-        self.mainlayout  = QtGui.QHBoxLayout()
-        self.setLayout(self.mainlayout)
-        self.mainlayout.setSpacing(0)
+        h = QtGui.QHBoxLayout()
+        mainlayout.addLayout(h)
+        h.addStretch()
+        self.combo_hearing_level_preset = QtGui.QComboBox()
+        h.addWidget(self.combo_hearing_level_preset)
+        self.combo_hearing_level_preset.addItems(hearing_level_preset.keys())
+        self.combo_hearing_level_preset.currentIndexChanged.connect(self.on_preset_changed)
+        self.combo_compression_loss_preset = QtGui.QComboBox()
+        h.addWidget(self.combo_compression_loss_preset)
+        self.combo_compression_loss_preset.addItems(compression_loss_preset.keys())
+        self.combo_compression_loss_preset.currentIndexChanged.connect(self.on_preset_changed)
         
-        self.band_widgets = []
         
-        self.set(band_values)
-    
-    def set(self, band_values):
-        for band_widget in self.band_widgets:
-            self.mainlayout.removeWidget(band_widget)
-            band_widget.deleteLater()
-            band_widget.setParent(None)
+        self.canvas = MplCanvas()
+        mainlayout.addWidget(self.canvas)
         
-        self.band_widgets = []
-        for band_value in band_values:
-            w = Band()
-            self.mainlayout.addWidget(w)
-            w.set(**band_value)
-            self.band_widgets.append(w)
-            w.valueChanged.connect(self.valueChanged.emit)
-
-    def get(self):
-        band_values = [w.get() for w in self.band_widgets]
-        return band_values
-
-
-class MyViewBox(pg.ViewBox):
-    def __init__(self, *args, **kwds):
-        pg.ViewBox.__init__(self, *args, **kwds)
-        self.disableAutoRange()
-    def mouseClickEvent(self, ev):
-        ev.accept()
-    def mouseDoubleClickEvent(self, ev):
-        ev.accept()
-    def mouseDragEvent(self, ev):
-        ev.ignore()
-    def wheelEvent(self, ev):
-        ev.accept()
-
-
-class HearingLossParameter(QtGui.QWidget):
-    valueChanged = QtCore.Signal()
-    def __init__(self, parent = None):
-        QtGui.QWidget.__init__(self, parent)
-        mainlayout = QtGui.QVBoxLayout()
-        self.setLayout(mainlayout)
         
         h = QtGui.QHBoxLayout()
         mainlayout.addLayout(h)
-        but = QtGui.QPushButton(u'Set flat')
-        h.addWidget(but)
-        but.clicked.connect(self.set_flat)
-        but = QtGui.QPushButton(u'Copy L>R')
-        h.addWidget(but)
-        but.clicked.connect(self.copy_l_to_r)
-        but = QtGui.QPushButton(u'Load')
-        h.addWidget(but)
-        but.clicked.connect(self.load)
-        but = QtGui.QPushButton(u'Save')
-        h.addWidget(but)
-        but.clicked.connect(self.save)
-        
-
-        self.tab = QtGui.QTabWidget()
-        mainlayout.addWidget(self.tab)
-        
-        ears = ('left', 'right')
-        self.eqs =[ ]
-        for i, ear in enumerate(ears):
-            eq = OneEq()
-            self.eqs.append(eq)
-            #~ mainlayout.addWidget(QtGui.QLabel(u'<h1><b>{}</b>'.format(ear)))
-            #~ mainlayout.addWidget(QtGui.QLabel(u'{}'.format(ear)))
-            #~ mainlayout.addWidget(eq)
-            self.tab.addTab(eq, ear)
+        h.addSpacerItem(QtGui.QSpacerItem(130,0))
+        g = QtGui.QGridLayout()
+        h.addLayout(g)
+        self.spin_hearinglevel = {}
+        self.spin_comp_degree = {}
+        for i in range(n):
+            g.addWidget(QtGui.QLabel('{} Hz'.format(freqs[i])), 0, i)
+            self.spin_hearinglevel[i] = s = pg.SpinBox(suffix='dBHL', bounds=[-20, 120], step=1)
+            g.addWidget(s, 1, i)
+            s.sigValueChanged.connect(self.on_spinbox_changed)
+            self.spin_comp_degree[i] = s = pg.SpinBox(suffix='%', bounds=[0,100], step=10)
+            g.addWidget(s, 2, i)
+            s.sigValueChanged.connect(self.on_spinbox_changed)
             
-            eq.valueChanged.connect(self.refresh_curves)
-            eq.valueChanged.connect(self.valueChanged.emit)
-        
-        self.viewBox = MyViewBox()
-        self.graphicsview  = pg.GraphicsView()
-        #~ mainlayout.addWidget(self.graphicsview)
-        self.tab.addTab(self.graphicsview, 'plot')
-        self.plot = pg.PlotItem(viewBox = self.viewBox)
-        self.plot.setLogMode(x=True, y = False)
-        self.graphicsview.setCentralItem(self.plot)
-        
-        for i, ear in enumerate(ears):
-            color = ['#FF0000', '#00FF00'][i]
-            curve = pg.PlotCurveItem(pen = color)
-            self.plot.addItem(curve)
+        h.addSpacerItem(QtGui.QSpacerItem(110,0))
 
-        #~ self.canvas = MplCanvas()
-        #~ mainlayout.addWidget(self.canvas)
+        self.hearing_level = hearing_level_preset['80 years']
         
-        #~ ax = self.canvas.ax
-        #~ self.lines = []
-        #~ for i, ear in enumerate(ears):
-            #~ colors = ['#FF0000', '#00FF00']
-            #~ self.lines.extend(ax.plot([], [], color= colors[i]))
+        self.compression_loss = compression_loss_preset['0%']
         
+        self.lines = plot_hearingloss(self.canvas.ax,  self.hearing_level, self.compression_loss)
         
-        self.set_configuration()
+        self.all_interp1d_loss_to_comp = {}
+        self.all_interp1d_comp_to_loss = {}
+        for i in range(n):
+            losses = [compression_loss_preset[k][i] for k in compression_ratio.keys()]
+            comps = list(compression_ratio.values())
+            self.all_interp1d_loss_to_comp[i] = scipy.interpolate.interp1d(losses, comps, kind='linear')
+            self.all_interp1d_comp_to_loss[i] = scipy.interpolate.interp1d(comps, losses, kind='linear')
+            
+        self.refresh_spinbox()
     
-    def set_configuration(self, loss_weigth = [_default_bands]*2,):
-        assert len(loss_weigth) ==2
-        for i, one_loss in enumerate(loss_weigth):
-            self.eqs[i].set(one_loss)
-        self.refresh_curves()
-    
-    def get_configuration(self):
-        config = {'loss_weigth' : [ eq.get() for eq in self.eqs] }
-        # TODO classer freqs
-        return config
-
-    def refresh_curves(self):
-        loss_weigth = self.get_configuration()['loss_weigth']
-        for i in range(2):
-            freqs = [0., ]
-            db_loss = [0., ]
-            for v in loss_weigth[i]:
-                freqs.append(v['freq'])
-                db_loss.append(v['db_loss'])
-            freqs += [23000., ]
-            db_loss += [0., ]                
-            freqs  = np.array(freqs)
-            db_loss  = np.array(db_loss)
-            f = scipy.interpolate.interp1d(freqs, db_loss, kind='linear')
-            x =np.arange(50,22000, 10)
-            #~ self.plot.items[i].setData(x, f(x))
-            
-            self.plot.items[i].setData(np.log10(x), f(x))
-            
-        #~ self.plot.setXRange(0, 23000)
-        self.plot.setXRange(np.log10(40), np.log10(23000))
-        self.plot.setYRange(-80, 0.)
-
-    #~ def refresh_curves(self):
-        #~ print('refresh_curves')
+    def refresh_canvas(self):
+        x = np.arange(n)+.5
+        compression_loss_clip = np.amin([self.hearing_level, self.compression_loss], axis=0)
         
-        #~ ax = self.canvas.ax
-        #~ ax.clear()
-        #~ loss_weigth = self.get_configuration()['loss_weigth']
-        #~ for i in range(2):
-            #~ freqs = [0., ]
-            #~ db_loss = [0., ]
-            #~ for v in loss_weigth[i]:
-                #~ freqs.append(v['freq'])
-                #~ db_loss.append(v['db_loss'])
-            #~ freqs += [23000., ]
-            #~ db_loss += [0., ]
-            #~ freqs  = np.array(freqs)
-            #~ db_loss  = np.array(db_loss)
-            #~ f = scipy.interpolate.interp1d(freqs, db_loss, kind='linear')
-            #~ x =np.arange(50,22000, 10)
-            #~ colors =  ['#FF0000', '#00FF00']
-            #~ ax.semilogx(x, f(x), color=colors[i])
-            
-        #~ ax.set_xlim(0, 23000)
-        #~ ax.set_ylim(-80, 0.)
-        #~ self.canvas.draw()
-
+        self.lines['hearing_level'].set_data(x, self.hearing_level)
+        self.lines['compression_loss_clip'].set_data(x, compression_loss_clip)
+        
+        self.canvas.draw()
     
-    def set_flat(self):
-        for eq in self.eqs:
-            eq.set(_default_bands)
-            #~ loss = eq.get()
-            #~ for e in loss:
-                #~ e['db_loss'] = 0.
-            #~ eq.set(loss)
-    def copy_l_to_r(self):
-        self.eqs[1].set(self.eqs[0].get())
+    def refresh_spinbox(self):
+        self.get_compression_degree()
+        for i in range(n):
+            s = self.spin_hearinglevel[i]
+            s.sigValueChanged.disconnect(self.on_spinbox_changed)
+            s.setValue(self.hearing_level[i])
+            s.sigValueChanged.connect(self.on_spinbox_changed)
+            
+            s = self.spin_comp_degree[i]
+            s.sigValueChanged.disconnect(self.on_spinbox_changed)
+            s.setValue(float(np.round(self.compression_degree[i]*100.)))
+            s.sigValueChanged.connect(self.on_spinbox_changed)
+    
+    def on_preset_changed(self):
+        k = self.combo_hearing_level_preset.currentText()
+        self.hearing_level = hearing_level_preset[k]
 
-    def load(self):
-        fd = QtGui.QFileDialog(fileMode= QtGui.QFileDialog.ExistingFile, acceptMode = QtGui.QFileDialog.AcceptOpen)
-        fd.setNameFilters(['Hearingloss setup (*.json)', 'All (*)'])
-        fd.setViewMode( QtGui.QFileDialog.Detail )
-        if fd.exec_():
-            filename = fd.selectedFiles()[0]
-            loss_weigth = json.load(open(filename, 'r', encoding='utf8'))
-            self.set_configuration(loss_weigth = loss_weigth)
-
-    def save(self):
-        fd = QtGui.QFileDialog(fileMode= QtGui.QFileDialog.AnyFile, acceptMode = QtGui.QFileDialog.AcceptSave, defaultSuffix = 'json')
-        fd.setNameFilter('Hearingloss setup (*.json)')
-        if fd.exec_():
-            filename = fd.selectedFiles()[0]
-            print(self.get_configuration()['loss_weigth'])
-            json.dump(self.get_configuration()['loss_weigth'],open(filename, 'w', encoding='utf8'), indent=4, separators=(',', ': '))
+        k = self.combo_compression_loss_preset.currentText()
+        self.compression_loss = compression_loss_preset[k]
+        
+        self.refresh_canvas()
+        self.refresh_spinbox()
+    
+    def on_spinbox_changed(self, sender):
+        self.hearing_level = [ float(self.spin_hearinglevel[i].value()) for i in range(n)]
+        if sender in self.spin_comp_degree.values():
+            self.get_compression_loss()
+        
+        self.refresh_canvas()
+        self.refresh_spinbox()
+        
+    def get_compression_degree(self):
+        compression_loss_clip = np.amin([self.hearing_level, self.compression_loss], axis=0)
+        self.compression_degree = []
+        for i in range(n):
+            self.compression_degree.append(float(self.all_interp1d_loss_to_comp[i](compression_loss_clip[i])))
+        return self.compression_degree
+    
+    def get_compression_loss(self):
+        for i in range(n):
+            v = float(self.spin_comp_degree[i].value())/100.
+            self.compression_loss[i] = self.all_interp1d_comp_to_loss[i](v)
+        
 
 
 if __name__ == '__main__':
