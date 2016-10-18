1e3986931e4a1dcafa13fc94517fc72ab572cdd1
Samuel Garcia <sam/garcia.die@gmail.com>
2016-10-18 10:43:41 +0200

    New try for autodoc in readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,6 +22,20 @@ import os
 
 sys.path.insert(0, os.path.abspath('../..'))
 
+## patch for read the doc
+# http://read-the-docs.readthedocs.io/en/latest/faq.html
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['scipy', 'numpy', 'pyopencl', 'sounddevice']
+#~ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+## end patch for read the doc
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
